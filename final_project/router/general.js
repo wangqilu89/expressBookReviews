23657721230c42498fb37f0b9994f7257e9dda56
Wang Qilu <wangqilu.89@gmail.com>
2025-02-13 22:52:50 -0500

    Itest

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,42 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.status(300).json(books[req.params.isbn]);
+
+    //return res.status(300).json({ISBN:req.params.isbn});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  var newObj = {}
+  Object.keys(books).forEach((key,index) => {
+    if (books[key]['author'] == req.params.author) {
+        newObj[key] = books[key]
+    }
+  })
+  return res.status(300).json(newObj);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  var newObj = {}
+  Object.keys(books).forEach((key,index) => {
+    if (books[key]['title'] == req.params.title) {
+        newObj[key] = books[key]
+    }
+  })
+  return res.status(300).json(newObj);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(req.params);
+  //return res.status(300).json(books[req.params.review]['reviews']);
 });
 
 module.exports.general = public_users;
